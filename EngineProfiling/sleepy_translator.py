54e0c046ed5832ed8671a73e860e7df2881c6ae4
Beherith <mysterme@gmail.com>
2020-10-03 11:07:49 +0200

    sleepy translator py2 to py3

--- EngineProfiling/sleepy_translator.py
+++ EngineProfiling/sleepy_translator.py
@@ -1,6 +1,6 @@
 description='HOW TO USE: \nCapture spring  with very sleepy CS, save .sleepy file\nDownload and unzip next to script the debug symbols from\n http://springrts.com/dl/buildbot/default/develop/101.0.1-63-g5ebf047/win32/ \nDownload engine source files from github\nRun script with -i [inputfile].sleepy -s C:/spring_source/spring/ \nOpen [inputfile_translated].sleepy in very sleepy CS'
 
-print description
+print(description)
 
 
 from optparse import OptionParser
@@ -20,7 +20,7 @@ parser.add_option('-d', '--dbgfile',action='store',dest='dbgfile',help='debug fi
 
 options, args=parser.parse_args()
 sleepytype='csv'
-print options
+print(options)
 
 dbgmax=100000000
 cnt=0
@@ -29,14 +29,14 @@ if '.sleepy' in options.input:
 	fh=open(options.input, 'rb') 
 	z=zipfile.ZipFile(fh)
 	for name in z.namelist():
-		print name
+		print(name)
 		z.extract(name, os.getcwd())
 
 	symbols=open('Symbols.txt')
 	sleepyfile=symbols.readlines()
 	symbols.close()
 else:
-	print 'failed to open',options.input
+	print('failed to open',options.input)
 
 
 
@@ -59,22 +59,22 @@ for sleepyline in sleepyfile:
 				address_count+=1		
 
 	except:
-		print 'Failed to parse line',sleepyline,'in',options.input,sleepyline.strip().split(' ')
+		print('Failed to parse line',sleepyline,'in',options.input,sleepyline.strip().split(' '))
 		traceback.print_exc()
 		pass
 		
 # print 'addresses:',to_translate
 cmd = ['addr2line.exe', '-e', options.dbgfile]
-print 'Command line: ' + ' '.join(cmd)
+print('Command line: ' + ' '.join(cmd))
 addr2line = Popen(cmd, stdin = PIPE, stdout = PIPE, stderr = PIPE)
 if addr2line.poll() == None:
 	stdout, stderr = addr2line.communicate('\n'.join(to_translate))
 else:
 	stdout, stderr = addr2line.communicate()
 if stderr:
-	print '%s stderr: %s' % (ADDR2LINE, stderr)
+	print('%s stderr: %s' % (ADDR2LINE, stderr))
 if addr2line.returncode != 0:
-	print ' fatal : %s exited with status %s' % (ADDR2LINE, addr2line.returncode)
+	print(' fatal : %s exited with status %s' % (ADDR2LINE, addr2line.returncode))
 print ('\t\t[OK]')
 # print stdout
 translated = stdout.split('\n')
@@ -83,9 +83,9 @@ if len(to_translate) <= len(translated):
 	for i in range(len(to_translate)):
 		result[to_translate[i]] = translated[i]
 else:
-	print 'Unmatched to_translate and tranlated lengths!',len(to_translate) , len(translated)
+	print('Unmatched to_translate and tranlated lengths!',len(to_translate) , len(translated))
 
-print 'Query successful, stacktrace results', len(result)
+print('Query successful, stacktrace results', len(result))
 
 
 def getcodeline(path, i): #fetches line i of the code, removes double quotes, replace with singles, max length of 80 chars
@@ -100,10 +100,10 @@ def getcodeline(path, i): #fetches line i of the code, removes double quotes, re
 			line=line[0:74]+'...'
 		#print 'got code line:',line
 		if len(line)<1:
-			print 'got suspiciosly short line',path,i,line
+			print('got suspiciosly short line',path,i,line)
 		return line
 	except:
-		print 'warning, cant fine code line',path,line
+		print('warning, cant fine code line',path,line)
 		pass
 		return ''
 	
@@ -121,7 +121,7 @@ for sleepyline in sleepyfile:
 			if options.sourcepath and 'build/default/../../' in newsourcefile:
 				
 				rawsourcefile = newsourcefile.partition('build/default/../../')[2]
-				print 'rawsourcefile',rawsourcefile
+				print('rawsourcefile',rawsourcefile)
 				path=options.sourcepath+rawsourcefile
 				shortname='<'+rawsourcefile.rpartition('/')[2]+'> ' # so we only have the filename like Unit.cpp
 				codeline=getcodeline(path, int(newsourceline))
@@ -138,24 +138,24 @@ for sleepyline in sleepyfile:
 		else:
 			outf.write(sleepyline)
 	except:
-		print 'Failed to translate line',sleepyline,'in',options.input
+		print('Failed to translate line',sleepyline,'in',options.input)
 		traceback.print_exc()
 		outf.write(sleepyline)
 		pass
-print 'Good lines=',goodline,' out of ', len (result)
+print('Good lines=',goodline,' out of ', len (result))
 outf.close() # GOD FUCKING DAMMIT!
 
 
 outfname=options.input.partition('.sleepy')[0]+'_translated.sleepy'
 
 cmd = 'del '+outfname
-print cmd
+print(cmd)
 os.system(cmd)
 # THE ORDER IN WHICH THESE FILES ARE ZIPPED DECIDES EVERYTHING!!!!!!!!!!!!!!!!!!
 cmd='zip -1 '+outfname+' Stats.txt Symbols.txt IPCounts.txt Callstacks.txt "Version 0.90 required"'
-print cmd
+print(cmd)
 os.system(cmd)
-print 'Done zipping'
+print('Done zipping')
 
 
 
