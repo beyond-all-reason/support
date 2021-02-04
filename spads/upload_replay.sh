c33852b8101967ed45e341f643b75a4ca431d99c
Jazcash <jaspercashmore@gmail.com>
2021-02-04 15:03:29 +0000

    fixed filepath

--- spads/upload_replay.sh
+++ spads/upload_replay.sh
@@ -4,7 +4,7 @@ demoName=$(basename -- "$1")
 
 sftp -i ~/.ssh/beherith@bar-rts.com -l 1000 -P 21344 -o "StrictHostKeyChecking no" beherith@bar-rts.com <<EOF
 cd /var/www/demos/unprocessed
-put "$demoName" "$demoName.filepart"
+put "$1" "$demoName.filepart"
 rename "$demoName.filepart" "$demoName"
 exit
 EOF
